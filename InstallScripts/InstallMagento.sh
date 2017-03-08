c297b1feaf73c5622abc39128f2d6b0f9578e867
rupeshnagar <rupesh.nagar@maaglabs.com>
2017-03-08 21:20:45 +0530

    Updated File After Installation

--- InstallScripts/InstallMagento.sh
+++ InstallScripts/InstallMagento.sh
@@ -8,14 +8,13 @@
 # $5 - magento SQL Password
 # $6 - magento admin first name
 # $7 -magento admin last name
-# $8 -magento admin surname
-# $9 -magneto admin email
-# $10 - magento admin usrname
-#$11 - magento admin pwd
-#$12 - magento connect public key
-#$13 -magento connect private key
+# $8 -magneto admin email
+# $9 - magento admin usrname
+#$10 - magento admin pwd
+#$11 - magento connect public key
+#$12 -magento connect private key
 #steps to install apache2
-if [ $1 ] && [ $2 ] && [ $3 ] && [ $4 ] && [ $5 ] && [ $6 ] && [ $7 ] && [ $8 ] && [ $9 ] && [ $10 ] && [ $11 ] && [ $12 ] && [ $13 ];
+if [ $1 ] && [ $2 ] && [ $3 ] && [ $4 ] && [ $5 ] && [ $6 ] && [ $7 ] && [ $8 ] && [ $9 ] && [ $10 ] && [ $11 ] && [ $12 ] ;
 then
   #Installbasic
   sudo apt-get install \
@@ -28,11 +27,10 @@ then
 sudo apt-get -y install apache2
 
 #install MYSQL
-sudo apt-get-y install mysql5.6
-sudo apt-get -y install mysql-server-5.6 mysql-client-5.6
+sudo apt-get -y install mysql-server-5.7 mysql-client-5.7
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $5"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $5"
-sudo apt-get install mysql-server-5.6 --yes
+#sudo apt-get install mysql-server-5.6 --yes
 mysql -u root --password="$5" -e"DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS $2; FLUSH PRIVILEGES; SHOW DATABASES;"
 
 #Install PHP
@@ -54,7 +52,7 @@ sudo apt-get install \
     php-zip \
     --yes
 
-service php7.0-fpm restart
+sudo service php7.0-fpm restart
 
 #Install PHP
 #sudo apt-get -y install php7
@@ -65,87 +63,60 @@ service php7.0-fpm restart
 #sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv
 
 #download composer and set
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer | sudo php
+sudo mv composer.phar /usr/local/bin/composer
 
  # Create a new user for magento
-sudo adduser $4 --gecos "Magento System,0,0,0" --disabled-password
+sudo adduser $3 --gecos "Magento System,0,0,0" --disabled-password
 echo "$3:$4" | sudo chpasswd
-usermod -g www-data $3
-mkdir /var/www/html/$2
+sudo usermod -g www-data $3
+sudo mkdir /var/www/html/$2
 
 #set rep.magento.com authendication options in order to get the details
- rm /var/www/html/$2/auth.json
- echo "{\"http-basic\":{\"repo.magento.com\":{\"username\":\""$12"\", \"password\":\""$13"\"}}}" >> /var/www/html/$2/auth.json
-
-#Create New Magento Database
-
-		
-#mysql -u root -p
-#create database magento;
-#GRANT ALL ON magento.* TO magento@localhost IDENTIFIED BY 'magento';
-
-
-
-#user composer to download the project repository. It  required to pass the  access keys which can be get by creating  account in https://marketplace.magento.com/customer/accessKeys/list/
-# by default it will copy the files in ir /home/serveradmin/project-community-edition if  required to  install at any other dir  give the dir name <INSTALL DIR NAME> at the end of the given command
-#$ composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition 
-
-$ composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/html/$2
+sudo composer config -g http-basic.repo.magento.com $11 $12
+sudo composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/html/$2
 
 # Create a new site configuration and add in apache for magento
-#sudo nano /etc/apache2/sites-available/magento.conf and add the following command
-
-#<VirtualHost *:80>
-#    DocumentRoot /var/www/html
-#    <Directory /var/www/html/>
-#        Options Indexes FollowSymLinks MultiViews
-#        AllowOverride All
-#    </Directory>
-#</VirtualHost>
-
-sudo echo "<VirtualHost *:80>\n
-    DocumentRoot /var/www/html\n
-    <Directory /var/www/html/>\n
-        Options Indexes FollowSymLinks MultiViews\n
-        AllowOverride All\n
-    </Directory>\n
-</VirtualHost>" >> /etc/apache2/sites-available/$2.conf
-
+sudo bash -c 'echo "<VirtualHost *:80>
+DocumentRoot /var/www/html
+<Directory /var/www/html/>
+Options Indexes FollowSymLinks MultiViews
+AllowOverride All
+</Directory>
+</VirtualHost>" >> /etc/apache2/sites-available/$2.conf'
 #enable the new site and 
 sudo a2ensite $2.conf
+sudo service apache2 reload
 
 #disable the default site
 sudo  a2dissite default
+sudo service apache2 reload
 #Go to installed php version apache2 php.ini file and update memory_limit to 2GB
 
 #change to add allow url rewite and handle phpencryption in apache
 sudo a2enmod rewrite
-sudo php5enmod mcrypt
+sudo service apache2 restart
+sudo phpenmod  mcrypt
+sudo service apache2 restart
+sudo a2enconf php7.0-fpm
+sudo service apache2 restart
 
 
-#install all files in dir /home/serveradmin/project-community-edition
+#install all files in  magento dir 
+cd /var/www/html/$2 && find var vendor pub/static pub/media app/etc -type f -exec sudo chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec sudo chmod u+w {} \; && sudo chmod u+x bin/magento
 
-find var vendor pub/static pub/media app/etc -type f -exec sudo chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec sudo  chmod u+w {} \; &&  sudo chmod u+x bin/magento
-
-
-#cd /home/serveradmin/project-community-edition && find var vendor pub/static pub/media app/etc -type f -exec chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec chmod u+w {} \; && chmod u+x bin/magento
-
-sudo cd /var/www/HTML/$2 && find var vendor pub/static pub/media app/etc -type f -exec chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec chmod u+w {} \; && chmod u+x bin/magento
-
-sudo cd /var/www/HTML/$2 && find var vendor pub/static pub/media app/etc -type f -exec chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec chmod u+w {} \; && chmod u+x bin/magento
-
-#after this go to /bin directory
-sudo cd /var/www/HTML/$2/bin
+#after this go to /bin directory of magento installation
+cd /var/www/html/$2/bin
 #give install command 
-php magento setup:install --base-url=http://$1.eastus.cloudapp.azure.com/$2/ \
---db-host=$3 --db-name=magento --db-user=root --db-password=$5 \
---admin-firstname=$6=User --admin-email=$9 \
---admin-user=$10 --admin-password=$11 --language=en_US \
+sudo php magento setup:install --base-url=http://$1.eastus.cloudapp.azure.com/$2/ \
+--db-host=localhost --db-name=$2 --db-user=root --db-password=$5 \
+--admin-firstname=$6 --admin-lastname=$7 --admin-email=$8 \
+--admin-user=$9 --admin-password=$10 --language=en_US \
 --currency=USD --timezone=America/Chicago --use-rewrites=1
 
 # give permission to web user  in apache2 www-data
 # go to magento installation directory
+cd /var/www/html/$2
 sudo chown -R www-data .
 
 # to run cron job
@@ -161,12 +132,11 @@ else
 		echo "5th parameter is  mysql SQL Password";
 		echo "6th parameter is magento admin first name";
 		echo "7th parameter is magento admin last name";
-		echo "8th parameter is magento admin surname";
-		echo "9th parameter is admin email";
-		echo "10th parameter is magento admin usrname";
-		echo "11th parameter is magento admin pwd";
-		echo "12th parameter is magento connect public key";
-		echo "13th parameter is magento connect private key";
+		echo "8th parameter is admin email";
+		echo "9th parameter is magento admin usrname";
+		echo "10th parameter is magento admin pwd";
+		echo "11th parameter is magento connect public key";
+		echo "12th parameter is magento connect private key";
         #echo "Try this: magento-prepare.sh 2.0.7 mywebshop.com magento magento";
         echo "";
 fi;
