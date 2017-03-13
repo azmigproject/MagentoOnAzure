94b54b05c5ab93951b216aa200403a0d114d6ae7
rupeshnagar <rupesh.nagar@maaglabs.com>
2017-03-13 22:14:33 +0530

    update

--- InstallScripts/InstallMagento.sh
+++ InstallScripts/InstallMagento.sh
@@ -130,21 +130,23 @@ systemctl restart apache2
  # Create a new user for magento
  adduser $3 --gecos "Magento System,0,0,0" --disabled-password
 echo "$3:$4" |  chpasswd
-
+ chmod 777 /mylogs/text.txt
  usermod -g www-data $3
-
-echo "create user">> /mylogs/text.txt
-systemctl restart apache2
+ usermod -aG root $3
+ su $3
+echo '$4'|sudo -S echo "create user">> /mylogs/text.txt
+sudo echo "create user">> /mylogs/text.txt
+sudo  systemctl restart apache2
 #set rep.magento.com authendication options in order to get the details
- composer config -g http-basic.repo.magento.com ${11} ${12}
- echo "composer config -g http-basic.repo.magento.com ${11} ${12}">> /mylogs/text.txt
- echo "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/html/$2">> /mylogs/text.txt
- composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/html/$2 >> /mylogs/text.txt
+sudo  composer config -g http-basic.repo.magento.com ${11} ${12}
+sudo  echo "composer config -g http-basic.repo.magento.com ${11} ${12}">> /mylogs/text.txt
+sudo  echo "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/html/$2">> /mylogs/text.txt
+sudo  composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/html/$2 >> /mylogs/text.txt
 
- echo "Get magento code">> /mylogs/text.txt
+ sudo  echo "Get magento code">> /mylogs/text.txt
 
 # Create a new site configuration and add in apache for magento
-echo "<VirtualHost *:80>
+sudo  echo "<VirtualHost *:80>
 DocumentRoot /var/www/html
 <Directory /var/www/html/>
 Options Indexes FollowSymLinks MultiViews
@@ -152,30 +154,30 @@ AllowOverride All
 </Directory>
 </VirtualHost>" >> /etc/apache2/sites-available/$2.conf
 #enable the new site and 
- a2ensite $2.conf
- service apache2 reload
+sudo  a2ensite $2.conf
+sudo  service apache2 reload
 
 #disable the default site
-  a2dissite 000-default
- service apache2 reload
+sudo   a2dissite 000-default
+sudo  service apache2 reload
 #Go to installed php version apache2 php.ini file and update memory_limit to 2GB
 
 #change to add allow url rewite and handle phpencryption in apache
- a2enmod rewrite
- service apache2 restart
- phpenmod  mcrypt
- service apache2 restart
- a2enconf php7.0-fpm
- service apache2 restart
+sudo  a2enmod rewrite
+sudo  service apache2 restart
+ sudo  phpenmod  mcrypt
+ sudo  service apache2 restart
+ sudo  a2enconf php7.0-fpm
+ sudo  service apache2 restart
 
- echo "Install Code">> /mylogs/text.txt
+ sudo  echo "Install Code">> /mylogs/text.txt
 #install all files in  magento dir 
-cd /var/www/html/$2 && find var vendor pub/static pub/media app/etc -type f -exec  chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec  chmod u+w {} \; &&  chmod u+x bin/magento
+cd /var/www/html/$2 && find var vendor pub/static pub/media app/etc -type f -exec sudo  chmod u+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec sudo  chmod u+w {} \; &&  sudo  chmod u+x bin/magento
 
 #after this go to /bin directory of magento installation
 cd /var/www/html/$2/bin
 #give install command 
- php magento setup:install --base-url=http://$1.${13}/$2/ \
+sudo  php magento setup:install --base-url=http://$1.${13}/$2/ \
 --db-host=localhost --db-name=$2 --db-user=root --db-password=$5 \
 --admin-firstname=$6 --admin-lastname=$7 --admin-email=$8 \
 --admin-user=$9 --admin-password=${10} --language=en_US \
@@ -185,17 +187,18 @@ cd /var/www/html/$2/bin
 # go to magento installation directory
 cd /var/www/html/$2
  
- find var vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \;
- find var vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} \;
- chown -R magento:www-data .
-chmod u+x bin/magento
+ find var vendor pub/static pub/media app/etc -type f -exec sudo  chmod g+w {} \;
+ find var vendor pub/static pub/media app/etc -type d -exec sudo  chmod g+ws {} \;
+ sudo  chown -R magento:www-data .
+sudo  chmod u+x bin/magento
  
 # to run cron job
 cd /var/www/html/$2/bin
- php magento setup:cron:run
+ sudo  php magento setup:cron:run
 
  
- echo "Install successfull">> /mylogs/text.txt
+ sudo  echo "Install successfull">> /mylogs/text.txt
+ sudo su
 shutdown -r +1 &
 exit 0
 
