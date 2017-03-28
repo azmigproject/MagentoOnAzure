6050fd05213c31bd84dc94a3ec0b389243a1697b
azmigproject <azmigproject@outlook.com>
2017-03-28 18:57:24 +0530

    Update

--- ExistingPackageDeployment/InstallScripts/InstallMagento.sh
+++ ExistingPackageDeployment/InstallScripts/InstallMagento.sh
@@ -6,15 +6,11 @@
 # $3 - magento user name
 # $4 - magento user password
 # $5 - magento SQL Password
-# $6 - magento admin first name
-# $7 -magento admin last name
-# $8 -magneto admin email
-# $9 - magento admin usrname
-#$10 - magento admin pwd
-#$11 - magento connect public key
-#$12 -magento connect private key
-#$13 -HOSTNAME
-#14 - MYSQL DB NAME
+#$6 -HOSTNAME
+#7 - MYSQL DB NAME
+#$8 - MagentoFileBackup
+#$9 - MagentoDBBackup
+#$10 - Magento DB that need to be backup
 #steps to install apache2
 mkdir /mylogs
 echo "testing">> /mylogs/text.txt
@@ -27,7 +23,7 @@ if [[ $(id -u) -ne 0 ]] ; then
     exit 1
 fi
 
-if [ $# < 14 ]; then
+if [ $# < 10 ]; then
      echo ""
         echo "Missing parameters.";
         echo "1st parameter is domain name";
@@ -35,21 +31,17 @@ if [ $# < 14 ]; then
         echo "3rd parameter is server linux user (it will create it)";
         echo "4th parameter is server linux password (it will create it)";
 		echo "5th parameter is  mysql SQL Password";
-		echo "6th parameter is magento admin first name";
-		echo "7th parameter is magento admin last name";
-		echo "8th parameter is admin email";
-		echo "9th parameter is magento admin usrname";
-		echo "10th parameter is magento admin pwd";
-		echo "11th parameter is magento connect public key";
-		echo "12th parameter is magento connect private key";
-		echo "13th parameter is HOSTNAME";
-		echo "14th parameter is  mysql SQL DB Name";
+		echo "6th parameter is HOSTNAME";
+		echo "7th parameter is  mysql SQL DB Name";
+		echo "8th parameter is  MagentoFileBackup";
+		echo "9th parameter is MagentoDBBackup";
+		echo "10th parameter is Magento DB that need to be backup";
         #echo "Try this: magento-prepare.sh 2.0.7 mywebshop.com magento magento";
         echo "";
     exit 1
 fi
 
-echo "domain name=$1 |Folder name =$2| magento user name=$3|magento user passwor=$4|magento SQL Password=$5|magento admin first name=$6|magento admin last name=$7| magneto admin email=$8|magento admin usrname=$9|magento admin pwd=${10}|magento connect public key=${11}| magento connect private key=${12}|HOSTNAME=${13}| magento DB Name=${14} ">> /mylogs/text.txt
+echo "domain name=$1 |Folder name =$2| magento user name=$3|magento user passwor=$4|magento SQL Password=$5|HOSTNAME=$6| mysql SQL DB Name=$7| MagentoFileBackup=$8| MagentoDBBackup=$9| MagentoDB That need to be restore=${10}">> /mylogs/text.txt
 apt-get update >> /mylogs/text.txt
 #Installbasic
    apt-get install \
@@ -69,9 +61,11 @@ echo "installed basic">> /mylogs/text.txt
  debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password $5"
  apt-get -y install mysql-server-5.7 mysql-client-5.7 >> /mylogs/text.txt
 # apt-get install mysql-server-5.6 --yes
-mysql -u root --password="$5" -e"DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS ${14}; FLUSH PRIVILEGES; SHOW DATABASES;" >> /mylogs/text.txt
+mysql -u root --password="$5" -e"DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS $7; FLUSH PRIVILEGES; SHOW DATABASES;" >> /mylogs/text.txt
 
-echo "installed MYSQL">> /mylogs/text.txt
+
+
+echo "installed MYSQL and New DB">> /mylogs/text.txt
 # Update apt-get 
 apt-get update >> /mylogs/text.txt
 
@@ -104,48 +98,84 @@ echo "installed PHP">> /mylogs/text.txt
 apt-get -y install curl php7.0-cli git
 echo "installed curl php7.0-cli git">> /mylogs/text.txt
 
-#download composer and set
-wget https://getcomposer.org/composer.phar -O composer.phar >> /mylogs/text.txt
-mv composer.phar /usr/local/bin/composer >> /mylogs/text.txt
-chmod 777  /usr/local/bin/composer >> /mylogs/text.txt
- echo "downloaded composer ">> /mylogs/text.txt
- composer>> /mylogs/text.txt
 systemctl restart apache2
+
+mkdir /MagentoBK
+chmod -R 777 /MagentoBK
+#download magento file backup
+echo "Start downloading magento backup files">> /mylogs/text.txt
+wget $8 -P /MagentoBK  >> /mylogs/text.txt
+MagentoBKFile=${8##*/}
+echo "Downloaded magento backup files. MagentoBKFile=$MagentoBKFile">> /mylogs/text.txt
+
+
+#download magento DB backup
+echo "Start downloading mangeto db backup files">> /mylogs/text.txt
+wget $9  -P  /MagentoBK >> /mylogs/text.txt
+MagentoDBBKFile=${9##*/}
+chmod -R 777 /MagentoBK
+echo "End downloading mangeto db backup files. MagentoDBBKFile=$MagentoDBBKFile">> /mylogs/text.txt
+
+#create directory where code will store
+
+mkdir /var/www/$2
+tar -xvf /MagentoBK/$MagentoBKFile -C /var/www/$2
+echo "End unziping magento files">> /mylogs/text.txt
+#Uninstall DB backup
+systemctl restart apache2
+mkdir /MagentoBK/DB
+chmod -R 777 /MagentoBK/DB
+#unzip /MagentoBK/$MagentoDBBKFile -d /MagentoBK/DB
+tar -xvf /MagentoBK/$MagentoDBBKFile -C /MagentoBK/DB
+
+mysql -u root --password="$5" -e" use $7; source /MagentoBK/DB/${10};" >> /mylogs/text.txt
+rm -rf /MagentoBK/DB
+#update DB with new website root path
+unsecurePath="http://$1.$6/"
+securePath="https://$1.$6/"
+mysql -u root --password="$5" -e " use $7; update mage_core_config_data set value='$unsecurePath' where path='web/unsecure/base_url'; update mage_core_config_data set value='$securePath' where path='web/secure/base_url';" >> /mylogs/text.txt
+
+#Remove folder having zip files
+echo "Removing downloaded zip files">> /mylogs/text.txt
+rm -rf /MagentoBK >> /mylogs/text.txt
+
+# Create a new site configuration and add in apache for magento
+echo "<VirtualHost *:80>
+	ServerName $1.$6
+        ServerAlias $1.$6
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/$2/2016080806
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+       CustomLog ${APACHE_LOG_DIR}/access.log combined
+	
+#<Directory /var/www/$2/2016080806>
+#	Options Indexes FollowSymLinks MultiViews
+#	AllowOverride All
+#</Directory>
+</VirtualHost>" >> /etc/apache2/sites-available/$2.conf
+
+
 
  # Create a new user for magento
  adduser $3 --gecos "Magento System,0,0,0" --disabled-password
 echo "$3:$4" |  chpasswd
  
  usermod -g www-data $3
- usermod -aG root $3
+ usermod -aG sudo $3
+  usermod -aG root $3
 
  su $3
 echo '$4'|sudo -S echo "create user">> /mylogs/text.txt
 sudo echo "create user">> /mylogs/text.txt
 sudo chmod -R 755 /var/www
 sudo  systemctl restart apache2
-#set rep.magento.com authendication options in order to get the details
-sudo  composer config -g http-basic.repo.magento.com ${11} ${12}
-sudo  echo "composer config -g http-basic.repo.magento.com ${11} ${12}">> /mylogs/text.txt
-sudo  echo "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/$2">> /mylogs/text.txt
-sudo  composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition  /var/www/$2 >> /mylogs/text.txt
 
- sudo  echo "Get magento code">> /mylogs/text.txt
+#install all files in  magento dir 
 
-# Create a new site configuration and add in apache for magento
-sudo  echo "<VirtualHost *:80>
-	ServerName $1.${13}
-        ServerAlias $1.${13}
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/$2
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-       CustomLog ${APACHE_LOG_DIR}/access.log combined
-	DocumentRoot /var/www/$2
-#<Directory /var/www/$2/>
-#	Options Indexes FollowSymLinks MultiViews
-#	AllowOverride All
-#</Directory>
-</VirtualHost>" >> /etc/apache2/sites-available/$2.conf
+cd /var/www/$2
+sudo chmod -R 777 /var/www/$2
+
+
 #enable the new site and 
 sudo  a2ensite $2.conf
 sudo  service apache2 reload
@@ -163,17 +193,15 @@ sudo  service apache2 restart
 sudo  a2enconf php7.0-fpm
 sudo  service apache2 restart
 sudo  echo "Install Code">> /mylogs/text.txt
-#install all files in  magento dir 
-#after this go to /bin directory of magento installation
-cd /var/www/$2/bin
-#give install command 
-sudo  echo "Running Install magento command">> /mylogs/text.txt
-sudo  php magento setup:install --base-url=http://$1.${13}/ \
---db-host=localhost --db-name=${14} --db-user=root --db-password=$5 \
---admin-firstname=$6 --admin-lastname=$7 --admin-email=$8 \
---admin-user=$9 --admin-password=${10} --language=en_US \
---currency=USD --timezone=America/Chicago --use-rewrites=1
-sudo  echo "magento installation complete">> /mylogs/text.txt
+
+#Update the database details in local files
+#filexml="app/etc/local.xml"
+#filephp="app/etc/env.php"
+#if [ -f "$filexml" ] 
+#else if [ -f "filephp" ] 
+#fi
+
+
 
 # give permission to web user  in apache2 www-data
 # go to magento installation directory
