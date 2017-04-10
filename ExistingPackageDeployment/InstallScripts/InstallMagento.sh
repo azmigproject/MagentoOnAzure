7995cac15a98e2cb2b2529180b7176b99845b4c1
azmigproject <azmigproject@outlook.com>
2017-04-10 18:27:37 +0530

    Updated Scripts having all changes

--- ExistingPackageDeployment/InstallScripts/InstallMagento.sh
+++ ExistingPackageDeployment/InstallScripts/InstallMagento.sh
@@ -11,6 +11,9 @@
 #$8 - MagentoFileBackup
 #$9 - MagentoDBBackup
 #$10 - Magento DB that need to be backup
+#$11 - Magento Media Folder backup
+#$12 - Magento Init Folder backup
+#$13 - Magento Var Folder backup
 #steps to install apache2
 mkdir /mylogs
 echo "testing">> /mylogs/text.txt
@@ -23,7 +26,7 @@ if [[ $(id -u) -ne 0 ]] ; then
     exit 1
 fi
 
-if [ $# < 10 ]; then
+if [ $# < 13 ]; then
      echo ""
         echo "Missing parameters.";
         echo "1st parameter is domain name";
@@ -36,12 +39,15 @@ if [ $# < 10 ]; then
 		echo "8th parameter is  MagentoFileBackup";
 		echo "9th parameter is MagentoDBBackup";
 		echo "10th parameter is Magento DB that need to be backup";
+		echo "11th parameter is Magento Media folder backup";
+		echo "12th parameter is Magento Init folder backup";
+		echo "13th parameter is Magento Var folder backup";
         #echo "Try this: magento-prepare.sh 2.0.7 mywebshop.com magento magento";
         echo "";
     exit 1
 fi
 
-echo "domain name=$1 |Folder name =$2| magento user name=$3|magento user passwor=$4|magento SQL Password=$5|HOSTNAME=$6| mysql SQL DB Name=$7| MagentoFileBackup=$8| MagentoDBBackup=$9| MagentoDB That need to be restore=${10}">> /mylogs/text.txt
+echo "domain name=$1 |Folder name =$2| magento user name=$3|magento user passwor=$4|magento SQL Password=$5|HOSTNAME=$6| mysql SQL DB Name=$7| MagentoFileBackup=$8| MagentoDBBackup=$9| MagentoDB That need to be restore=${10}| MagentoDB Media folder backup=${11}| MagentoDB Init folder backup=${12}| MagentoDB Var folder backup=${13}">> /mylogs/text.txt
 apt-get update >> /mylogs/text.txt
 #Installbasic
    apt-get install \
@@ -49,98 +55,57 @@ apt-get update >> /mylogs/text.txt
     curl \
     unzip \
     --yes
-	
-echo "installed basic">> /mylogs/text.txt
+
 #Install Apache
  apt-get -y install apache2
-
-echo "installed Apache">> /mylogs/text.txt
-
-apt-get -y install software-properties-common
-apt-get update
-
-
-#install MYSQL 
- debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password $5"
- debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password $5"
- apt-get -y install mysql-server-5.5 mysql-client-5.6 >> /mylogs/text.txt
-# apt-get install mysql-server-5.6 --yes
-mysql -u root --password="$5" -e"DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS $7; FLUSH PRIVILEGES; SHOW DATABASES;" >> /mylogs/text.txt
-
-
-
-echo "installed MYSQL and New DB">> /mylogs/text.txt
-
-apt-get -y install software-properties-common
-add-apt-repository -y ppa:ondrej/php
-apt-get update
-apt-get -y install php5.6 >> /mylogs/text.txt
-apt-get -y install php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml >> /mylogs/text.txt
-
-# Update apt-get 
-apt-get update >> /mylogs/text.txt
-
-#Install PHP
-
-apt-get install \
-    php5.6-fpm \
-    php5.6-mysql \
-    php5.6-mcrypt \
-    php5.6-curl \
-   php5.6-cli \
-    php5.6-gd \
-    php5.6-xsl \
-    php5.6-json \
-   php5.6-intl \
-    php5.6-dev \
-    php5.6-common \
-    php5.6-soap \
-    php5.6-mbstring \
-    php5.6-zip \
-    --yes >> /mylogs/text.txt
-
-
- #apt-get install \
-  #  php-fpm \
-   # php-mysql \
-    #php-mcrypt \
-    #php-curl \
-    #php-cli \
-   # php-gd \
-   # php7.0-xsl \
-    #php-json \
-    #php-intl \
-    #php-pear \
-    #php-dev \
-    #php-common \
-    #php-soap \
-    #php-mbstring \
-    #php-zip \
-    #--yes >> /mylogs/text.txt
-apt-get update >> /mylogs/text.txt
-a2enmod proxy_fcgi setenvif >> /mylogs/text.txt
-a2enconf php5.6-fpm >> /mylogs/text.txt
-service php5.6-fpm restart >> /mylogs/text.txt
-apt-get -y install apache2 php5.6 libapache2-mod-php5.6 >> /mylogs/text.txt
-#a2enmod proxy_fcgi setenvif >> /mylogs/text.txt
-#a2enconf php7.0-fpm >> /mylogs/text.txt
-#service php7.0-fpm restart >> /mylogs/text.txt
-#apt-get -y install apache2 php7.0 libapache2-mod-php7.0 >> /mylogs/text.txt
-service apache2 restart >> /mylogs/text.txt
-echo "installed PHP">> /mylogs/text.txt
-  
-#apt-get -y install curl php7.0-cli git
-#echo "installed curl php7.0-cli git">> /mylogs/text.txt
-
-systemctl restart apache2
-
+echo "installed basic">> /mylogs/text.txt
+#First create the folder where tar file will be downloaded
 mkdir /MagentoBK
+#download magento media folder backup
+echo "Start downloading magento media folder backup files">> /mylogs/text.txt
+wget ${11} -P /MagentoBK  >> /mylogs/text.txt
+MagentoMediaBKFile=${11##*/}
+echo "Downloaded magento media folder backup files. MagentoMediaBKFile=$MagentoMediaBKFilee">> /mylogs/text.txt
 chmod -R 777 /MagentoBK
+
+#create directory where code will store
+echo "Created required directory and Start downloading magento media folder backup files">> /mylogs/text.txt
+
+mkdir /var/www/$2
+tar -xvf /MagentoBK/$MagentoMediaBKFile -C /var/www/$2
+rm -rf /MagentoBK/$MagentoMediaBKFile >> /mylogs/text.txt
+
+echo "Completed downloaded for magento media folder backup files and remove the backup file">> /mylogs/text.txt	
+
 #download magento file backup
 echo "Start downloading magento backup files">> /mylogs/text.txt
 wget $8 -P /MagentoBK  >> /mylogs/text.txt
 MagentoBKFile=${8##*/}
 echo "Downloaded magento backup files. MagentoBKFile=$MagentoBKFile">> /mylogs/text.txt
+chmod -R 777 /MagentoBK
+tar -xvf /MagentoBK/$MagentoInitBKFile -C /var/www/$2
+rm -rf /MagentoBK/$MagentoInitBKFilee >> /mylogs/text.txt
+echo "unzip magento backup files">> /mylogs/text.txt
+
+#download magento init folder backup
+echo "Start downloading magento init folder backup files">> /mylogs/text.txt
+wget ${12} -P /MagentoBK  >> /mylogs/text.txt
+MagentoInitBKFile=${12##*/}
+echo "Downloaded magento init folder backup files. MagentoInitBKFile=$MagentoInitBKFile">> /mylogs/text.txt
+chmod -R 777 /MagentoBK
+tar -xvf /MagentoBK/$MagentoInitBKFile -C /var/www/$2
+rm -rf /MagentoBK/$MagentoInitBKFilee >> /mylogs/text.txt
+echo "unzip magento init folder">> /mylogs/text.txt
+
+#download magento var folder backup
+echo "Start downloading magento var folder backup files">> /mylogs/text.txt
+wget ${13} -P /MagentoBK  >> /mylogs/text.txt
+MagentoVarBKFile=${13##*/}
+echo "Downloaded magento var folder backup files. MagentoVarBKFile=$MagentoVarBKFile">> /mylogs/text.txt
+chmod -R 777 /MagentoBK
+tar -xvf /MagentoBK/$MagentoVarBKFile -C /var/www/$2
+rm -rf /MagentoBK/$MagentoVarBKFile >> /mylogs/text.txt
+echo "Unzip magento var folder">> /mylogs/text.txt
 
 
 #download magento DB backup
@@ -150,18 +115,67 @@ MagentoDBBKFile=${9##*/}
 chmod -R 777 /MagentoBK
 echo "End downloading mangeto db backup files. MagentoDBBKFile=$MagentoDBBKFile">> /mylogs/text.txt
 
-#create directory where code will store
 
-mkdir /var/www/$2
-tar -xvf /MagentoBK/$MagentoBKFile -C /var/www/$2
-echo "End unziping magento files">> /mylogs/text.txt
+
+
+
+echo "installed Apache">> /mylogs/text.txt
+
+#install MYSQL 
+ debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $5"
+ debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $5"
+ apt-get -y install mysql-server-5.5 mysql-client-5.5 >> /mylogs/text.txt
+# apt-get install mysql-server-5.6 --yes
+mysql -u root --password="$5" -e"DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS $7; FLUSH PRIVILEGES; SHOW DATABASES;" >> /mylogs/text.txt
+
+
+
+echo "installed MYSQL and New DB">> /mylogs/text.txt
+
+apt-get update
+apt-get -y install php5 >> /mylogs/text.txt
+#apt-get -y install php5.5-mbstring php5.5-mcrypt php5.5-mysql php5.5-xml >> /mylogs/text.txt
+
+# Update apt-get 
+apt-get update >> /mylogs/text.txt
+
+
+
+
+#Install PHP
+
+
+apt-get -y install \
+  php5-fpm \
+ php5-mysql \
+php5-mcrypt \
+php5-curl \
+php5-cli \
+php5-gd \
+ php5-xsl \
+   php5-json \
+   php5-intl \
+  
+   php5-dev \
+   php5-common \ 
+    >> /mylogs/text.txt
+apt-get update >> /mylogs/text.txt
+a2enmod proxy_fcgi setenvif >> /mylogs/text.txt
+a2enconf php5-fpm >> /mylogs/text.txt
+service php5-fpm restart >> /mylogs/text.txt
+apt-get -y install apache2 php5 libapache2-mod-php5 >> /mylogs/text.txt
+service apache2 restart >> /mylogs/text.txt
+echo "installed PHP">> /mylogs/text.txt
+service  apache2 restart
+
+
+echo "End unziping magento files and removed corresponding tar files">> /mylogs/text.txt
 #Uninstall DB backup
-systemctl restart apache2
+service  apache2 restart
 mkdir /MagentoBK/DB
 chmod -R 777 /MagentoBK/DB
-#unzip /MagentoBK/$MagentoDBBKFile -d /MagentoBK/DB
 tar -xvf /MagentoBK/$MagentoDBBKFile -C /MagentoBK/DB
-
+chmod -R 777 /MagentoBK/DB
 mysql -u root --password="$5" -e" use $7; source /MagentoBK/DB/${10};" >> /mylogs/text.txt
 rm -rf /MagentoBK/DB
 #update DB with new website root path
@@ -174,10 +188,18 @@ mysql -u root --password="$5" -e "
 echo "Removing downloaded zip files">> /mylogs/text.txt
 rm -rf /MagentoBK >> /mylogs/text.txt
 
+#Replace the deatabase details in local.xml file
+sed -i 's/74.208.174.2/localhost/g' /var/www/$2/.init/local.xml >> /mylogs/text.txt
+sed -i 's/aat01_www/$7/g' /var/www/$2/.init/local.xml >> /mylogs/text.txt
+sed -i 's/aat01/root/g' /var/www/$2/.init/local.xml >> /mylogs/text.txt
+sed -i 's/DiplVYtpSM0XeuKU/$5/g' /var/www/$2/.init/local.xml >> /mylogs/text.txt
+
+echo "updated local.xml file">> /mylogs/text.txt
+
 # Create a new site configuration and add in apache for magento
 echo "<VirtualHost *:80>
-	ServerName $1.$6
-        ServerAlias $1.$6
+	ServerName http://$1.$6/
+        ServerAlias  http://$1.$6/
         ServerAdmin webmaster@localhost
         DocumentRoot /var/www/$2/2016080806
         ErrorLog ${APACHE_LOG_DIR}/error.log
@@ -196,7 +218,7 @@ echo "$3:$4" |  chpasswd
 echo '$4'|sudo -S echo "create user">> /mylogs/text.txt
 sudo echo "create user">> /mylogs/text.txt
 sudo chmod -R 755 /var/www
-sudo  systemctl restart apache2
+sudo  service apache2 restart
 
 #install all files in  magento dir 
 
@@ -218,33 +240,37 @@ sudo  a2enmod rewrite
 sudo  service apache2 restart
 sudo  phpenmod  mcrypt
 sudo  service apache2 restart
-sudo  a2enconf php5.6-fpm
+sudo  a2enconf php5-fpm
 sudo  service apache2 restart
 sudo  echo "Install Code">> /mylogs/text.txt
-
-#Update the database details in local files
-#filexml="app/etc/local.xml"
-#filephp="app/etc/env.php"
-#if [ -f "$filexml" ] 
-#else if [ -f "filephp" ] 
-#fi
 
 
 
 # give permission to web user  in apache2 www-data
 # go to magento installation directory
-cd /var/www/$2/ 
+cd /var/www/$2/2016080806 
 sudo  echo "start giving permissions">> /mylogs/text.txt 
-find var vendor pub/static pub/media app/etc -type f -exec sudo  chmod g+w {} \; >> /mylogs/text.txt
-find var vendor pub/static pub/media app/etc -type d -exec sudo  chmod g+ws {} \; >> /mylogs/text.txt
+find var app/etc -type f -exec sudo  chmod g+w {} \; >> /mylogs/text.txt
+find var app/etc -type d -exec sudo  chmod g+ws {} \; >> /mylogs/text.txt
 sudo  chown -R $3:www-data . >> /mylogs/text.txt
-sudo  chmod u+x bin/magento >> /mylogs/text.txt
+sudo chmod -R o+w media var >> /mylogs/text.txt
+sudo chmod o+w app/etc >> /mylogs/text.txt
+sudo chmod 550 mage >> /mylogs/text.txt
 sudo  echo "end giving permissions">> /mylogs/text.txt
 
-# to run cron job
-cd /var/www/$2/bin
-#sudo  php magento setup:cron:run
-sudo chmod -R 777 /var/www/$2
+#sudo chmod -R 777 /var/www/$2/2016080806 
+find . -type f -exec sudo chmod 644 {} \; >> /mylogs/text.txt
+find . -type d -exec sudo chmod 755 {} \; >> /mylogs/text.txt
+sudo chmod 550 mage >> /mylogs/text.txt
+sudo chmod -R 777  var >> /mylogs/text.txt
+sudo chmod -R 777 .var >> /mylogs/text.txt
+sudo chmod -R 777 pub/static >> /mylogs/text.txt
+sudo chmod -R 777 pub/media >> /mylogs/text.txt
+sudo chmod -R 777  media >> /mylogs/text.txt
+sudo chmod -R 777 .media >> /mylogs/text.txt
+cd /var/www/$2
+sudo chmod -R 777 .var >> /mylogs/text.txt
+sudo chmod -R 777 .media >> /mylogs/text.txt
 sudo  echo "started cron">> /mylogs/text.txt
 sudo  echo "Install successfull">> /mylogs/text.txt
 sudo su
