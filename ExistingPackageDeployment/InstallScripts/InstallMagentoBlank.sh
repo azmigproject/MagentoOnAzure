22d1aef25e7b00463608254923b70e6edaa46738
azmigproject <azmigproject@outlook.com>
2017-06-28 16:05:31 +0530

    Update

--- ExistingPackageDeployment/InstallScripts/InstallMagentoBlank.sh
+++ ExistingPackageDeployment/InstallScripts/InstallMagentoBlank.sh
@@ -20,7 +20,7 @@
 #$17- customerID
 #$18- customertier
 #$19- resourcegroup
-#$20- parameter is Monitoring tool files"
+#$20- parameter is Monitoring tool files
 
 #steps to install apache2
 mkdir /mylogs
@@ -75,21 +75,25 @@ apt-get -y -qq update
 #Install Apache
  apt-get -y  -qq install apache2
 echo "installed basic">> /mylogs/text.txt
+
 #First create the folder where tar file will be downloaded
 mkdir /MagentoBK
+
 #download magento media folder backup
 echo "Start downloading magento media folder backup files">> /mylogs/text.txt
 wget "${11}" -P /MagentoBK  -q
 MagentoMediaBKFile=${11##*/}
 echo "Downloaded magento media folder backup files. MagentoMediaBKFile=$MagentoMediaBKFile">> /mylogs/text.txt
 chmod -R 777 /MagentoBK
+
 #create directory where code will store
 echo "Created required directory and Start downloading magento media folder backup files">> /mylogs/text.txt
 mkdir /var/www/"$2"
 unzip /MagentoBK/"$MagentoMediaBKFile" -d /var/www/"$2"
 rm -rf /MagentoBK/"$MagentoMediaBKFile" 
 echo "Completed downloaded for magento media folder backup files and remove the backup file
-      Start downloading magento backup files">> /mylogs/text.txt	
+      Start downloading magento backup files">> /mylogs/text.txt
+	  
 #download magento file backup
 wget "$8" -P /MagentoBK -q
 MagentoBKFile=${8##*/}
@@ -99,6 +103,7 @@ tar -xvf /MagentoBK/"$MagentoBKFile" -C /var/www/"$2"
 rm -rf /MagentoBK/"$MagentoBKFile"
 echo "unzip magento backup files
 	 Start downloading magento init folder backup files">> /mylogs/text.txt
+
 #download magento init folder backup
 wget "${12}" -P /MagentoBK -q
 MagentoInitBKFile=${12##*/}
@@ -108,6 +113,7 @@ tar -xvf /MagentoBK/"$MagentoInitBKFile" -C /var/www/"$2"
 rm -rf /MagentoBK/"$MagentoInitBKFile"
 echo "unzip magento init folder
 	  Start downloading magento var folder backup files">> /mylogs/text.txt
+
 #download magento var folder backup
 wget "${13}" -P /MagentoBK -q
 MagentoVarBKFile=${13##*/}
@@ -117,21 +123,25 @@ tar -xvf /MagentoBK/"$MagentoVarBKFile" -C /var/www/"$2"
 rm -rf /MagentoBK/"$MagentoVarBKFile"
 echo "Unzip magento var folder
 	Start downloading mangeto db backup files">> /mylogs/text.txt
+
 #download magento DB backup
 wget "$9"  -P  /MagentoBK -q
 MagentoDBBKFile=${9##*/}
 chmod -R 777 /MagentoBK
 echo "End downloading mangeto db backup files. MagentoDBBKFile=$MagentoDBBKFile">> /mylogs/text.txt
+
 #install MYSQL 
  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $5"
  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $5"
  apt-get -y -qq install mysql-server-5.5 mysql-client-5.5
+
 # apt-get install mysql-server-5.6 --yes
 mysql -u root --password="$5" -e "DELETE FROM mysql.user WHERE User=' '; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS $7; FLUSH PRIVILEGES; SHOW DATABASES;"
 echo "installed MYSQL and New DB">> /mylogs/text.txt
 apt-get -y -qq update
 apt-get -y -qq install php5
 apt-get -y -qq update
+
 #Install PHP
 apt-get -y -qq install \
   php5-fpm \
@@ -144,8 +154,7 @@ php5-gd \
    php5-json \
    php5-intl \
      php5-dev \
-   php5-common 
-      
+   php5-common       
 apt-get -y -qq update
 a2enmod proxy_fcgi setenvif
 a2enconf php5-fpm
@@ -154,11 +163,13 @@ apt-get -y install apache2 php5 libapache2-mod-php5
 service  apache2 restart
 echo "installed PHP
 	  End unziping magento files and removed corresponding tar files">> /mylogs/text.txt
+
 #Uninstall DB backup
 service  apache2 restart
 mkdir /MagentoBK/DB
 tar -xvf /MagentoBK/"$MagentoDBBKFile" -C /MagentoBK/DB
 chmod -R 777 /MagentoBK/DB
+
 #Replace the template1 name to the name of domain in magento_init.sql file
 sed -i "s/template1.westus.cloudapp.azure.com/$1.$6/g" /MagentoBK/DB/magento_init.sql
 mysql -u root --password="$5" -e  " use $7; source /MagentoBK/DB/${10};" 
@@ -179,6 +190,7 @@ sed -i "s/aat01_www/$7/g" /var/www/"$2"/.init/local.xml
 sed -i "s/aat01/root/g" /var/www/"$2"/.init/local.xml
 sed -i "s/DiplVYtpSM0XeuKU/$5/g" /var/www/"$2"/.init/local.xml
 echo "updated local.xml file">> /mylogs/text.txt
+
 # Create a new site configuration and add in apache for magento
 echo "<VirtualHost *:80>
 	ServerName $1.$6
@@ -194,6 +206,7 @@ echo "<VirtualHost *:80>
                 allow from all
         </Directory>
 </VirtualHost>" >> /etc/apache2/sites-available/"$2".conf
+
 # Check if .htaccess file is Missing than add it from default location
 cd /var/www/"$2"/2016080806 || exit
 if [ ! -f ".htaccess" ]; then
@@ -201,6 +214,7 @@ if [ ! -f ".htaccess" ]; then
  wget "${14}" -q
  fi
  cd / || exit
+
  # Create a new user for magento
  adduser "$3" --gecos "Magento System,0,0,0" --disabled-password
 echo "$3:$4" |  chpasswd
@@ -212,17 +226,21 @@ echo "$4"|sudo -S echo "create user"
 echo "create user" | sudo tee -a /mylogs/text.txt > /dev/null
 sudo chmod -R 755 /var/www
 sudo  service apache2 restart
+
 #install all files in  magento dir 
 cd /var/www/"$2" || exit
 sudo chmod -R 777 /var/www/"$2"
+
 #enable the new site and 
 sudo  a2ensite "$2".conf
 sudo  service apache2 reload
+
 #disable the default site
 sudo   a2dissite 000-default
 sudo  service apache2 reload
+
 #Go to installed php version apache2 php.ini file and update memory_limit to 2GB
-#change to add allow url rewite and handle phpencryption in apache
+#change to add allow url rewrite and handle phpencryption in apache
 sudo  a2enmod rewrite
 sudo  service apache2 restart
 sudo  php5enmod  mcrypt
@@ -230,6 +248,7 @@ sudo  service apache2 restart
 sudo  a2enconf php5-fpm
 sudo  service apache2 restart
 echo "Install Code" | sudo tee -a /mylogs/text.txt > /dev/null
+
 # give permission to web user  in apache2 www-data
 # go to magento installation directory
 cd /var/www/"$2"/2016080806 || exit
@@ -241,6 +260,7 @@ sudo chmod -R o+w media var
 sudo chmod o+w app/etc 
 sudo chmod 550 mage 
 echo "end giving permissions" | sudo tee -a /mylogs/text.txt > /dev/null
+
 #sudo chmod -R 777 /var/www/$2/2016080806 
 find . -type f -exec sudo chmod 644 {} \; 
 find . -type d -exec sudo chmod 755 {} \; 
@@ -265,15 +285,16 @@ apt-get -y -qq update
 apt-get -y -qq install python-pip
 echo "Installed Python-Pip functionality
 	  Installing email functionality">> /mylogs/text.txt
+
 # section to install email service
 apt-get -y -qq install mailutils
 apt-get -y -qq install ssmtp
+
 #section for installing certbot SSL
 apt-get -y -qq install software-properties-common
 add-apt-repository -y  ppa:certbot/certbot
 apt-get -y -qq update
 apt-get -y -qq install python-certbot-apache 
-
 
 #Install Monitoring tools
 apt-get -y -qq install xinetd
@@ -291,61 +312,78 @@ rm -rf /MagentoBK
 #Remove folder having zip files
 echo "Removing downloaded zip files"
 
+# Cron Tab Update
+# New cron job
 
-#cron Tab Update
+mkdir -p /var/www/$2/2016080806/shell/synchronization/ && touch /var/www/$2/2016080806/shell/synchronization/processlock_main.txt
+
+mkdir -p /var/www/$2/2016080806/shell/synchronization/vehicle/ && touch /var/www/$2/2016080806/shell/synchronization/vehicle/ processlock_va.txt
+
+echo " #!/bin/bash
+echo 'starting MAIN script'
+cd /var/www/$2/2016080806/shell/synchronization/; /usr/bin/php main.php > /var/www/$2/2016080806/var/log/main_cron.log">>/var/www/$2/2016080806/shell/synchronization/start_main.sh
+
+
+echo " #!/bin/bash
+echo 'starting VA script'
+cd /var/www/$2/2016080806/shell/synchronization/vehicle/; python va_controller.py > /var/www/$2/2016080806/var/log/va_controller.log" >>/var/www/$2/2016080806/shell/synchronization/start_va.sh
+
+echo "*/10 *  *   *    *       flock -xn /var/www/$2/2016080806/shell/synchronization/processlock_main.txt -c /var/www/$2/2016080806/shell/synchronization/start_main.sh
+*/15 *  *   *    *       flock -xn /var/www/$2/2016080806/shell/synchronization/processlock_va.txt -c /var/www/$2/2016080806/shell/synchronization/start_va.sh
+*/10 *  *   *    *        cd /var/www/$2/2016080806/shell/synchronization/order/; /usr/bin/php syncOrder.php"
+
 echo "*/10 *  *   *    *      cd /var/www/$2/2016080806/shell/synchronization/; /usr/bin/php main.php > /var/www/$2/2016080806/var/log/main_cron.log
 */15 *  *   *    *      cd /var/www/$2/2016080806/shell/synchronization/vehicle/; python va_controller.py > /var/www/$2/2016080806/var/log/va_controller_cron.log
+*/10 *  *   *    *      cd /var/www/$2/2016080806/shell/synchronization/order/; /usr/bin/php syncOrder.php > /var/www/$2/2016080806/var/log/syncOrder.log
 " >>/etc/crontab
 
-sed -i 's,/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin,/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/var/www/$2/2016080806/shell/synchronization:/usr/bin,g' /etc/crontab
+sed -i "s,/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin,/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/var/www/$2/2016080806/shell/synchronization:/usr/bin,g" /etc/crontab
 
  crontab -l > Magentocron
  echo  "*/10   *   *    *   *   cd /var/www/$2/2016080806/shell/synchronization/; /usr/bin/php main.php > /var/www/$2/2016080806/var/log/main_cron.log" >> Magentocron
  echo  "*/15   *   *    *   *   cd /var/www/$2/2016080806/shell/synchronization/vehicle/; python va_controller.py > /var/www/$2/2016080806/var/log/va_controller_cron.log" >> Magentocron
+ echo  "*/10   *   *    *   *      cd /var/www/$2/2016080806/shell/synchronization/order/; /usr/bin/php syncOrder.php > /var/www/$2/2016080806/var/log/syncOrder.log" >> Magentocron
  crontab  Magentocron
  rm Magentocron
 
 mv /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.sample
 echo  "# Config file for sSMTP sendmail
-#
-# The person who gets all mail for userids < 1000
-# Make this empty to disable rewriting.
-#root=postmaster
-root=information-prod@gcommerceinc.com
-
-# The place where the mail goes. The actual machine name is required no
-# MX records are consulted. Commonly mailhosts are named mail.domain.com
-# mailhub=mail
-mailhub=smtp.office365.com:587
-AuthUser=information-prod@gcommerceinc.com
-AuthPass=AutoGComm1!
-UseTLS=YES
-UseSTARTTLS=YES
-TLS_CA_File=/etc/pki/tls/certs/ca-bundle.crt
-# Where will the mail seem to come from?
-#rewriteDomain=
-rewriteDomain=gcommerceinc.com
-
-# The full hostname
-hostname=$1.wdnmczgigfhudmf4p1sa3we05e.dx.internal.cloudapp.net
-#hostname=information-prod@gcommerceinc.com
-# Are users allowed to set their own From: address?
-# YES - Allow the user to specify their own From: address
-# NO - Use the system generated From: address
-FromLineOverride=YES" > /etc/ssmtp/ssmtp.conf
+	   #
+	   # The person who gets all mail for userids < 1000
+       # Make this empty to disable rewriting.
+       # root=postmaster
+         root=information-prod@gcommerceinc.com
+       # The place where the mail goes. The actual machine name is required no
+       # MX records are consulted. Commonly mailhosts are named mail.domain.com
+       # mailhub=mail
+         mailhub=smtp.office365.com:587
+         AuthUser=information-prod@gcommerceinc.com
+         AuthPass=AutoGComm1!
+         UseTLS=YES
+         UseSTARTTLS=YES
+         TLS_CA_File=/etc/pki/tls/certs/ca-bundle.crt
+       # Where will the mail seem to come from?
+       # rewriteDomain=
+         rewriteDomain=gcommerceinc.com
+	   # The full hostname
+         hostname=$1.wdnmczgigfhudmf4p1sa3we05e.dx.internal.cloudapp.net
+       # Are users allowed to set their own From: address?
+       # YES - Allow the user to specify their own From: address
+       # NO - Use the system generated From: address
+         FromLineOverride=YES" > /etc/ssmtp/ssmtp.conf
 
 mv /etc/ssmtp/revaliases /etc/ssmtp/revaliases.sample
 
 echo  "
-# sSMTP aliases
-#
-# Format:       local_account:outgoing_address:mailhub
-#
-# Example: root:your_login@your.domain:mailhub.your.domain[:port]
-# where [:port] is an optional port number that defaults to 25.
-root:information-prod@gcommerceinc.com:smtp.office365.com:587
-noreply:information-prod@gcommerceinc.com:smtp.office365.com:587
-" > /etc/ssmtp/revaliases
+		# sSMTP aliases
+		#
+		# Format:       local_account:outgoing_address:mailhub
+		#
+		# Example: root:your_login@your.domain:mailhub.your.domain[:port]
+		# where [:port] is an optional port number that defaults to 25.
+		  root:information-prod@gcommerceinc.com:smtp.office365.com:587
+		  noreply:information-prod@gcommerceinc.com:smtp.office365.com:587
+      " > /etc/ssmtp/revaliases
 END=$(date +%s)
 DIFFMin=$((((END - START )/60)))
 DIFFSec=$((((END - START )%60)))
@@ -377,14 +415,9 @@ VM Admin Pass:  ${16}"
     echo "$MailBody"
 } | ssmtp azuredeployments@gcommerceinc.com 
 
-
 echo "Mail Send. Install successfull">> /mylogs/text.txt
-
 chmod -R 777 var/www/"$2"/2016080806/shell/synchronization
-
 echo -n "user_id=${17};pmp2_url=http://gcommercepmp2.cloudapp.net/" >/var/www/"$2"/2016080806/app/etc/cfg/client_info.conf
-
 chmod 777 /var/www/"$2"/2016080806/app/etc/cfg/client_info.conf
-
 shutdown -r +1 &
 exit 0
