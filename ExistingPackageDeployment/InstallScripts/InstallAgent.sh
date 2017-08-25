f3ec488b97da6f4097012871697cb526b7961d11
azmigproject <azmigproject@outlook.com>
2017-08-25 11:46:58 +0530

    Update

--- ExistingPackageDeployment/InstallScripts/InstallAgent.sh
+++ ExistingPackageDeployment/InstallScripts/InstallAgent.sh
@@ -22,11 +22,18 @@ if [ $# -lt 6 ]; then
 fi
 apt-get  -y install libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev uuid-dev unzip
 apt-get update
-apt-get install -y python-software-properties debconf-utils
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" |  debconf-set-selections
-apt-get install -y oracle-java8-installer
+jvstatus=$(java -version)
+substring="version"
+if [ "$jvstatus" = "${jvstatus%$substring*}" ]; then
+ 
+ apt-get install default-jre
+
+fi
+# apt-get install -y python-software-properties debconf-utils
+# add-apt-repository -y ppa:webupd8team/java
+# apt-get update
+#echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" |  debconf-set-selections
+#apt-get install -y oracle-java8-installer
 
 wget "https://github.com/Microsoft/vsts-agent/releases/download/v2.120.1/vsts-agent-ubuntu.14.04-x64-2.120.1.tar.gz" -P /home/$3  -q
 
@@ -37,10 +44,11 @@ MgDir=$( find /var/www/ -type d -name "2016080806")
  chmod 755 -R  "$MgDir/shell"
  chmod 755 -R  "$MgDir/skin"
 
-mkdir "/mgbackup"
+mkdir "mgbackup"
 mkdir myagent && cd myagent
-su -c  "sudo tar zxvf /home/$3/vsts-agent-ubuntu.14.04-x64-2.120.1.tar.gz
-        ./config.sh --unattended --acceptteeeula --url \"$6\" --auth PAT --token \"$1\" --pool \"$2\" --agent \"$4\" --work \"$5\" " "$3"
+sudo tar zxvf /home/$3/vsts-agent-ubuntu.14.04-x64-2.120.1.tar.gz
+chmod 777 /myagent
+su -c  "./config.sh --unattended --acceptteeeula --url \"$6\" --auth PAT --token \"$1\" --pool \"$2\" --agent \"$4\" --work \"$5\" " "$3"
 
 echo "*  *  *   *    *      /myagent/bin/Agent.Listener run \$* & > /myagent/log.txt " >>/etc/crontab
 crontab -l > Magentocron
