405954a85615a3349c6ce16bc5525bdd90d76b11
azmigproject <azmigproject@outlook.com>
2017-09-05 16:12:14 +0530

    Update Package

--- ExistingPackageDeployment/InstallScripts/InstallAgentCentOS.sh
+++ ExistingPackageDeployment/InstallScripts/InstallAgentCentOS.sh
@@ -23,7 +23,8 @@ fi
 
 yum -y install libunwind.x86_64 icu deltarpm epel-release unzip libunwind gettext libcurl-devel openssl-devel zlib libicu-devel
 
-yum -y update
+#yum -y update
+yum update -y --exclude=WALinuxAgent
 
 #wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 #"http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jre-8u144-linux-x64.rpm"
