85f683acc35d052d4299f0b86efd34ff9320f1ed
azmigproject <azmigproject@outlook.com>
2017-12-20 11:23:58 +0530

    Update InstallMagentoBlankCentOS.sh

--- ExistingPackageDeployment/InstallScripts/InstallMagentoBlankCentOS.sh
+++ ExistingPackageDeployment/InstallScripts/InstallMagentoBlankCentOS.sh
@@ -6,7 +6,7 @@
 # $3 - magento user name
 # $4 - magento user password
 # $5 - magento SQL Password
-#$6 -  HOSTNAME
+#$6 -HOSTNAME
 #$7 - MYSQL DB NAME
 #$8 - MagentoFileBackup
 #$9 - MagentoDBBackup
@@ -27,7 +27,7 @@
 # $24 - SenderDomain
 # $25 - tfsAccessToken
 # $26 - tfsAgentPool
-
+# $27 - magentoScriptFoldersBackup
 
 #steps to install apache2
 mkdir /mylogs
@@ -41,7 +41,7 @@ if [[ $(id -u) -ne 0 ]] ; then
     exit 1
 fi
 
-if [ $# -lt 26 ]; then
+if [ $# -lt 27 ]; then
      echo ""
         echo "Missing parameters.";
         echo "1st parameter is domain name";
@@ -70,37 +70,32 @@ if [ $# -lt 26 ]; then
 		echo "24th parameter is SenderDomain";
 		echo "25th parameter is TFS Access Token for setting Agent in remote machine";
 		echo "26th parameter is TFS Agent Pool Name for listing agent in the pool in TFS";
+		echo "27th parameter is magento New Folders Backup file name for listing new folders that need to be replaced";
         #echo "Try this: magento-prepare.sh 2.0.7 mywebshop.com magento magento";
         echo "";
     exit 1
 fi
 
-sed -i 's/Defaults   requiretty/Defaults   !requiretty/g' /etc/sudoers
-START="$(date +%s)" 
-echo "StartTime=$START |
-domain name=$1 |Folder name =$2| magento user name=$3|magento user passwor=$4|magento SQL Password=$5|HOSTNAME=$6| mysql SQL DB Name=$7| MagentoFileBackup=$8| MagentoDBBackup=$9| MagentoDB That need to be restore=${10}| MagentoDB Media folder backup=${11}| MagentoDB Init folder backup=${12}| MagentoDB Var folder backup=${13}| htaccess location=${14}">> /mylogs/text.txt
-yum -y -q install php httpd php-mcrypt php-xml php-xml php-devel php-fpm php-json php-intl php-dev php-common unzip git curl 
-yum -y -q install php-imap php-soap php-mbstring php-simplexml  
-yum -y -q install php-dom php-gd php-pear php-pecl-imagick php-pecl-apc php-magickwand 
-yum -y -q install gd gd-devel php-gd httpd-devel gcc curl php-curl mod_ssl pcre-devel 
-wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum -y -q install mysql mysql-server php-mysql git-core screen  
-yum -y -q install epel-release 
-yum -y -q install python-pip 
+START=$(date +%s)
+echo "StartTime=$START | domain name=$1 |Folder name =$2| magento user name=$3|magento user passwor=$4|magento SQL Password=$5|HOSTNAME=$6| mysql SQL DB Name=$7| MagentoFileBackup=$8| MagentoDBBackup=$9| MagentoDB That need to be restore=${10}| MagentoDB Media folder backup=${11}| MagentoDB Init folder backup=${12}| MagentoDB Var folder backup=${13}| htaccess location=${14}">> /mylogs/text.txt
+apt-get -y -qq update 
+#Installbasic
+   apt-get -qq install \
+    git \
+    curl \
+    unzip \
+	--yes
 
-echo "installed basic and required softwares like php  httpd(apache) mysql sSMTP for mails and other required packages
-	  Installed Python-Pip functionality
-	  Installing email functionality	">> /mylogs/text.txt
 #Install Apache
-yum -y -q install httpd
+ apt-get -y  -qq install apache2
+echo "installed basic">> /mylogs/text.txt
 
 #First create the folder where tar file will be downloaded
 mkdir /MagentoBK
 
 #download magento media folder backup
 echo "Start downloading magento media folder backup files">> /mylogs/text.txt
-wget "${11}" -P /MagentoBK -q
+wget "${11}" -P /MagentoBK  -q
 MagentoMediaBKFile=${11##*/}
 echo "Downloaded magento media folder backup files. MagentoMediaBKFile=$MagentoMediaBKFile">> /mylogs/text.txt
 chmod -R 777 /MagentoBK
@@ -111,70 +106,112 @@ mkdir /var/www/"$2"
 unzip /MagentoBK/"$MagentoMediaBKFile" -d /var/www/"$2"
 rm -rf /MagentoBK/"$MagentoMediaBKFile" 
 echo "Completed downloaded for magento media folder backup files and remove the backup file
-	  Start downloading magento backup files">> /mylogs/text.txt
+      Start downloading magento backup files">> /mylogs/text.txt
 	  
 #download magento file backup
-wget "$8" -P /MagentoBK  -q
+wget "$8" -P /MagentoBK -q
 MagentoBKFile=${8##*/}
 echo "Downloaded magento backup files. MagentoBKFile=$MagentoBKFile">> /mylogs/text.txt
 chmod -R 777 /MagentoBK
 tar -xvf /MagentoBK/"$MagentoBKFile" -C /var/www/"$2"
-rm -rf /MagentoBK/"$MagentoBKFile" 
+rm -rf /MagentoBK/"$MagentoBKFile"
 echo "unzip magento backup files
-     Start downloading magento init folder backup files">> /mylogs/text.txt
+	 Start downloading magento init folder backup files">> /mylogs/text.txt
+
+#download magento latest folders
+wget "${27}" -P /MagentoBK -q
+MGNewFolderFile=${27##*/}
+echo "Downloaded magento folder backup files. MGNewFolderFile=$MGNewFolderFile">> /mylogs/text.txt
+chmod -R 777 /MagentoBK
+mkdir /MagentoBK/NewFolder
+tar -xvf /MagentoBK/"$MGNewFolderFile" -C /MagentoBK/NewFolder
+echo "unzip magento backup files
+	Start downloading magento init folder backup files">> /mylogs/text.txt
 
 #download magento init folder backup
-wget "${12}" -P /MagentoBK  -q
+wget "${12}" -P /MagentoBK -q
 MagentoInitBKFile=${12##*/}
 echo "Downloaded magento init folder backup files. MagentoInitBKFile=$MagentoInitBKFile">> /mylogs/text.txt
 chmod -R 777 /MagentoBK
 tar -xvf /MagentoBK/"$MagentoInitBKFile" -C /var/www/"$2"
-rm -rf /MagentoBK/"$MagentoInitBKFile" 
+rm -rf /MagentoBK/"$MagentoInitBKFile"
 echo "unzip magento init folder
 	  Start downloading magento var folder backup files">> /mylogs/text.txt
 
 #download magento var folder backup
-wget "${13}" -P /MagentoBK  -q
+wget "${13}" -P /MagentoBK -q
 MagentoVarBKFile=${13##*/}
 echo "Downloaded magento var folder backup files. MagentoVarBKFile=$MagentoVarBKFile">> /mylogs/text.txt
 chmod -R 777 /MagentoBK
 tar -xvf /MagentoBK/"$MagentoVarBKFile" -C /var/www/"$2"
-rm -rf /MagentoBK/"$MagentoVarBKFile" 
+rm -rf /MagentoBK/"$MagentoVarBKFile"
 echo "Unzip magento var folder
-	  Start downloading mangeto db backup files">> /mylogs/text.txt
+	Start downloading mangeto db backup files">> /mylogs/text.txt
 
 #download magento DB backup
-wget "$9"  -P  /MagentoBK  -q
+wget "$9"  -P  /MagentoBK -q
 MagentoDBBKFile=${9##*/}
 chmod -R 777 /MagentoBK
-echo "End downloading mangeto db backup files. MagentoDBBKFile=$MagentoDBBKFile
-	 installed Apache">> /mylogs/text.txt
-service mysqld start
- mysqladmin -u root password "$5"
-systemctl enable mysqld.service
-systemctl enable httpd.service
+echo "End downloading mangeto db backup files. MagentoDBBKFile=$MagentoDBBKFile">> /mylogs/text.txt
 
-# yum install mysql-server-5.6 --yes
+#remove the folders from magento installation and copy the new folder their
+
+rm -rf /var/www/"$2"/"2016080806/app"
+rm -rf /var/www/"$2"/"2016080806/js"
+rm -rf /var/www/"$2"/"2016080806/shell"
+rm -rf /var/www/"$2"/"2016080806/skin"
+rm -rf /var/www/"$2"/"2016080806/var"
+
+mv  /MagentoBK/NewFolder/2016080806/app  /var/www/"$2"/"2016080806"/
+mv  /MagentoBK/NewFolder/2016080806/js  /var/www/"$2"/"2016080806"/
+mv  /MagentoBK/NewFolder/2016080806/shell  /var/www/"$2"/"2016080806"/
+mv  /MagentoBK/NewFolder/2016080806/skin  /var/www/"$2"/"2016080806"/
+mv  /MagentoBK/NewFolder/2016080806/var  /var/www/"$2"/"2016080806"/
+
+#install MYSQL 
+ debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $5"
+ debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $5"
+ apt-get -y -qq install mysql-server-5.5 mysql-client-5.5
+
+# apt-get install mysql-server-5.6 --yes
 mysql -u root --password="$5" -e "DELETE FROM mysql.user WHERE User=' '; DROP DATABASE IF EXISTS test; CREATE DATABASE IF NOT EXISTS $7; FLUSH PRIVILEGES; SHOW DATABASES;"
 mysql -u root --password="$5" -e " Grant ALL on *.* To 'root'@'localhost'; FLUSH PRIVILEGES;"
 mysql -u root --password="$5" -e " GRANT ALL PRIVILEGES ON *.* TO 'root' IDENTIFIED BY '$5' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 echo "installed MYSQL and New DB">> /mylogs/text.txt
-service php-fpm restart 
-systemctl stop httpd
+apt-get -y -qq update
+apt-get -y -qq install php5
+apt-get -y -qq update
 
-#yum -y install apache2 php5 libapache2-mod-php5 >> /mylogs/text.txt
-yum -y -q install httpd mod_fcgid php-cli
-echo "installed PHP">> /mylogs/text.txt
-service httpd restart
-echo "End unziping magento files and removed corresponding tar files">> /mylogs/text.txt
+#Install PHP
+apt-get -y -qq install \
+  php5-fpm \
+ php5-mysql \
+php5-mcrypt \
+php5-curl \
+php5-cli \
+php5-gd \
+ php5-xsl \
+   php5-json \
+   php5-intl \
+     php5-dev \
+   php5-common       
+apt-get -y -qq update
+a2enmod proxy_fcgi setenvif
+a2enconf php5-fpm
+service php5-fpm restart
+apt-get -y install apache2 php5 libapache2-mod-php5
+service  apache2 restart
+echo "installed PHP
+	  End unziping magento files and removed corresponding tar files">> /mylogs/text.txt
 
 #Uninstall DB backup
+service  apache2 restart
 mkdir /MagentoBK/DB
 tar -xvf /MagentoBK/"$MagentoDBBKFile" -C /MagentoBK/DB
 chmod -R 777 /MagentoBK/DB
 
 #Replace the template1 name to the name of domain in magento_init.sql file
-sed -i "s/template1.westus.cloudapp.azure.com/$1.$6/g" /MagentoBK/DB/magento_init.sql 
+sed -i "s/template1.westus.cloudapp.azure.com/$1.$6/g" /MagentoBK/DB/magento_init.sql
 mysql -u root --password="$5" -e  " use $7; source /MagentoBK/DB/${10};" 
 rm -rf /MagentoBK/DB
 
@@ -190,104 +227,115 @@ update `mage_core_config_data` SET value = 'email-smtp.us-west-2.amazonaws.com' 
 update `mage_core_config_data` SET value = '587' where path ='smtppro/general/smtp_port';
 update `mage_core_config_data` SET value = 'tls' where path ='smtppro/general/smtp_ssl';
 
- # if testing locally please comment below Mysql command
-   mysql -u root --password="$5" -e   "use $7; update magento.mage_core_config_data
-   set value = 'https://autosoez.azureedge.net/${17}/' where path = 'web/secure/base_media_url';"   
 
-#Remove folder having zip files
-echo "Removing downloaded zip files">> /mylogs/text.txt
-rm -rf /MagentoBK 
+# if testing locally please comment below Mysql command
+   mysql -u root --password="$5" -e   "use $7; update magento.mage_core_config_data
+   set value = 'https://autosoez.azureedge.net/${17}/' where path in( 'web/secure/base_media_url','web/unsecure/base_media_url');"  
 
 #Replace the database details in local.xml file
-sed -i "s/74.208.174.2/localhost/g" /var/www/"$2"/.init/local.xml 
-sed -i "s/aat01_www/$7/g" /var/www/"$2"/.init/local.xml 
-sed -i "s/aat01/root/g" /var/www/"$2"/.init/local.xml 
-sed -i "s/DiplVYtpSM0XeuKU/$5/g" /var/www/"$2"/.init/local.xml 
+sed -i "s/74.208.174.2/localhost/g" /var/www/"$2"/.init/local.xml
+sed -i "s/aat01_www/$7/g" /var/www/"$2"/.init/local.xml
+sed -i "s/aat01/root/g" /var/www/"$2"/.init/local.xml
+sed -i "s/DiplVYtpSM0XeuKU/$5/g" /var/www/"$2"/.init/local.xml
 echo "updated local.xml file">> /mylogs/text.txt
+
+cp -ar /var/www/"$2"/.init/local.xml /var/www/"$2"/"2016080806"/app/etc
+cp  -ar /var/www/"$2"/.init/config.xml /var/www/"$2"/"2016080806"/app/etc
 
 # Create a new site configuration and add in apache for magento
 echo "<VirtualHost *:80>
-	    ServerName http://$1.$6/
-        ServerAlias  http://$1.$6/
+	ServerName $1.$6
+        ServerAlias  $1.$6
         ServerAdmin webmaster@localhost
         DocumentRoot /var/www/$2/2016080806
-        ErrorLog logs/$1.$6-error.log
-       CustomLog logs/$1.$6-access.log combined
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+       CustomLog ${APACHE_LOG_DIR}/access.log combined
 	   <Directory /var/www/$2/2016080806/>
                 Options Indexes FollowSymLinks MultiViews
                 AllowOverride All
                 Order allow,deny
                 allow from all
-       </Directory>
-
-</VirtualHost>" >> /etc/httpd/conf.d/"$2".conf
+        </Directory>
+</VirtualHost>" >> /etc/apache2/sites-available/"$2".conf
 
 # Check if .htaccess file is Missing than add it from default location
-
 cd /var/www/"$2"/2016080806 || exit
-
 if [ ! -f ".htaccess" ]; then
-
- echo "copying htaccess file">> /mylogs/text.txt;
-
+ echo "copying htaccess file" >> /mylogs/text.txt;
  wget "${14}" -q
-
  fi
  cd / || exit
 
  chmod -R 755 /var/www
- service httpd restart
+ service apache2 restart
+
 #install all files in  magento dir 
 cd /var/www/"$2" || exit
  chmod -R 777 /var/www/"$2"
-  service httpd reload
- service httpd restart
-echo "Install Code"|  tee -a /mylogs/text.txt > /dev/null
+
+#enable the new site and 
+ a2ensite "$2".conf
+  service apache2 reload
+
+#disable the default site
+  a2dissite 000-default
+ service apache2 reload
+
+#Go to installed php version apache2 php.ini file and update memory_limit to 2GB
+#change to add allow url rewrite and handle phpencryption in apache
+ a2enmod rewrite
+ service apache2 restart
+ php5enmod  mcrypt
+  service apache2 restart
+ a2enconf php5-fpm
+ service apache2 restart
+echo "Install Code" | tee -a /mylogs/text.txt > /dev/null
+
 # give permission to web user  in apache2 www-data
 # go to magento installation directory
-cd /var/www/"$2"/2016080806  || exit
-echo "start giving permissions"|  tee -a /mylogs/text.txt > /dev/null
-find var app/etc -type f -exec chmod g+w {} \; 
-find var app/etc -type d -exec chmod g+ws {} \; 
-  chown -R "$3":www-data . 
+cd /var/www/"$2"/2016080806 || exit
+echo "start giving permissions" | tee -a /mylogs/text.txt > /dev/null
+find var app/etc -type f -exec chmod g+w {} \;
+find var app/etc -type d -exec chmod g+ws {} \;
+ chown -R "$3":www-data .
  chmod -R o+w media var 
  chmod o+w app/etc 
  chmod 550 mage 
-echo "end giving permissions" |  tee -a /mylogs/text.txt > /dev/null
+echo "end giving permissions" | tee -a /mylogs/text.txt > /dev/null
 
 find . -type f -exec chmod 644 {} \; 
 find . -type d -exec chmod 755 {} \; 
  chmod 550 mage 
  chmod -R 777  var 
+ #chmod -R 777 .var 
  chmod -R 777 pub/static 
  chmod -R 777 pub/media 
  chmod -R 777  media 
+# chmod -R 777 .media 
 cd /var/www/"$2" || exit
-
-echo "started cron" |  tee -a /mylogs/text.txt > /dev/null
+ #chmod -R 777 .var 
+# chmod -R 777 .media 
+#cd /var/www/"$2"/ || exit
+# rm -rf .var/cache/*
+echo "started cron" | tee -a /mylogs/text.txt > /dev/null
 
 IP=$(curl ipinfo.io/ip)
-echo "Installing certbot functionality">> /mylogs/text.txt
-systemctl stop httpd
-yum -y -q install python-certbot-apache 
-service httpd restart
-echo "Installed certbot functionality">> /mylogs/text.txt
+echo "Installing Python-Pip functionality">> /mylogs/text.txt
+apt-get -y -qq install epel-release 
+apt-get -y -qq update 
+apt-get -y -qq install python-pip
+echo "Installed Python-Pip functionality
+	  Installing email functionality">> /mylogs/text.txt
 
-if [ "${6/'azure.com'}" = "$6" ] ; then
-  certbot certonly --webroot -w /var/www/"$2"/2016080806/ -d "$1.$6"  --agree-tos  --email azuredeployments@gcommerceinc.com -n 
-else
-  certbot certonly --webroot -w /var/www/"$2"/2016080806/ -d "$1.$6"  --agree-tos  --email azuredeployments@gcommerceinc.com -n --test-cert 
-fi
-tempvar="$1.$6"
-certbot --apache -d "$tempvar" --no-redirect --agree-tos  --email azuredeployments@gcommerceinc.com  -n 
-certbot renew -n --agree-tos --email azuredeployments@gcommerceinc.com --post-hook "service apache2 restart" 
-echo " #!/bin/sh
-certbot renew -n --agree-tos --email azuredeployments@gcommerceinc.com --post-hook 'service apache2 restart'"> /etc/cron.daily/certbotcron
-chmod 777 /etc/cron.daily/certbotcron
+
+#section for installing certbot SSL
+apt-get -y -qq install software-properties-common
+add-apt-repository -y  ppa:certbot/certbot
+apt-get -y -qq update
+apt-get -y -qq install python-certbot-apache 
 
 #Install Monitoring tools
-yum -y -q install xinetd
-mkdir MagentoBK
+apt-get -y -qq install xinetd
 wget "${20}" -P /MagentoBK -q
 unzip /MagentoBK/MonitoringAgentFiles -d /MagentoBK/
 mv  /MagentoBK/check_mk_agent  /usr/bin  
@@ -296,22 +344,27 @@ mv  /MagentoBK/waitmax /usr/bin
 chmod +x  /usr/bin/waitmax 
 mv /MagentoBK/check_mk /etc/xinetd.d
 /etc/init.d/xinetd restart
-rm -rf /MagentoBK 
-# End Monitoring tools
+rm -rf /MagentoBK
+#end Monitoring tools
+
+#Remove folder having zip files
+echo "Removing downloaded zip files"
 
 # Cron Tab Update
 # New cron job
 
 mkdir -p /var/www/$2/2016080806/shell/synchronization/ && touch /var/www/$2/2016080806/shell/synchronization/processlock_main.txt
+
 mkdir -p /var/www/$2/2016080806/shell/synchronization/vehicle/ && touch /var/www/$2/2016080806/shell/synchronization/vehicle/ processlock_va.txt
+
 chmod +x /var/www/$2/2016080806/shell/synchronization/main.php 
 chmod +x /var/www/$2/2016080806/shell/synchronization/start_main.sh
-chmod +x /var/www/$2/2016080806/shell/synchronization/start_va.sh
+chmod +x /var/www/$2/2016080806/shell/synchronization/start_va.sh 
 chmod +x /var/www/$2/2016080806/shell/reindex.php
 
 echo " #!/bin/bash
 echo 'starting MAIN script'
-cd /var/www/$2/2016080806/shell/synchronization/; /usr/bin/php main.php > /var/www/$2/2016080806/var/log/main_cron.log"> /var/www/$2/2016080806/shell/synchronization/start_main.sh
+cd /var/www/$2/2016080806/shell/synchronization/; /usr/bin/php main.php > /var/www/$2/2016080806/var/log/main_cron.log">/var/www/$2/2016080806/shell/synchronization/start_main.sh
 
 echo " #!/bin/bash
 echo 'starting VA script'
@@ -338,15 +391,13 @@ sed -i "s,/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin,/usr/loca
  crontab  Magentocron
  rm Magentocron
 
-# section to install email service
- yum -y -q install ssmtp
- yum -y -q install mailx
+ apt-get -y -qq install mailutils
+ apt-get -y -qq install ssmtp
 
-# MailSendingVariables
-# Live
+
+
 
 echo "root="${21}"
-TLS_CA_File=/etc/pki/tls/certs/ca-bundle.crt
 mailhub=smtp.office365.com:587
 rewriteDomain="${24}"
 hostname=$1.wdnmczgigfhudmf4p1sa3we05e.dx.internal.cloudapp.net
@@ -356,16 +407,15 @@ AuthUser="${21}"
 AuthPass="${22}"
 AuthMethod=LOGIN
 FromLineOverride=YES" > /etc/ssmtp/ssmtp.conf
+
+
+
 echo "root:${21}:smtp.office365.com:587
 		  noreply:${21}:smtp.office365.com:587" > /etc/ssmtp/revaliases
-systemctl stop postfix
-systemctl disable postfix
-		  
-alternatives --set mta "/usr/sbin/sendmail.ssmtp"
 
-END="$(date +%s)"
-DIFFMin="$((((END - START )/60)))"
-DIFFSec="$((((END - START )%60)))"
+END=$(date +%s)
+DIFFMin=$((((END - START )/60)))
+DIFFSec=$((((END - START )%60)))
 
 MailBody="
 AutoSoEz Client Deployment Complete. Details given below<BR>
@@ -382,6 +432,7 @@ Customer Tier:  ${18}<BR>
 MySQL Password:   $5<BR>
 VM Admin User:  ${15}<BR>
 VM Admin Pass:  ${16}"
+
 {
     echo "To: ${23}"
     echo "From: noreply <${21}>"
@@ -391,13 +442,16 @@ VM Admin Pass:  ${16}"
 	echo "Content-Transfer-Encoding: 7bit;"
     echo
     echo "$MailBody"
-} | ssmtp ${23}
+} | ssmtp ${23} 
 
-echo "Mail Send. Install successfull">> /mylogs/text.txt 
+echo "Mail Send. Install successfull">> /mylogs/text.txt
 chmod -R 777 /var/www/"$2"/2016080806/shell/synchronization
 echo -n "user_id=${17};pmp2_url=http://gcommercepmp2.cloudapp.net/" >/var/www/"$2"/2016080806/app/etc/cfg/client_info.conf
 chmod 777 /var/www/"$2"/2016080806/app/etc/cfg/client_info.conf
 rm -rf /var/www/"$2"/2016080806/var/cache/*
-yum -y install htop
+ apt-get install htop
+wget "https://raw.githubusercontent.com/azmigproject/MagentoOnAzure/master/ExistingPackageDeployment/InstallScripts/InstallAgent.sh"
+chmod 777 InstallAgent.sh
 mkdir /var/tfsworkfolder
-curl https://raw.githubusercontent.com/azmigproject/MagentoOnAzure/master/ExistingPackageDeployment/InstallScripts/InstallAgentCentOS.sh | bash -s "${25}" "${26}" "${15}" "agent${17}${18}" "/var/tfsworkfolder" 'https://gcommerceinc.visualstudio.com'
+echo '/InstallAgent.sh "${25}" "${26}" "${15}" "agent${17}${18}" "/var/tfsworkfolder" https://gcommerceinc.visualstudio.com'>> /mylogs/text.txt
+./InstallAgent.sh "${25}" "${26}" "${15}" "agent${17}${18}" "/var/tfsworkfolder" https://gcommerceinc.visualstudio.com >> /mylogs/text.txt
